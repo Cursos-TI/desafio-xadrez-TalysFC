a5cc0d9be17e5603e8c283b1228b0337329e9b7d
TalysFC <talyschermont14@gmail.com>
2025-09-03 16:34:13 +0000

    alteração para nível mestre

--- xadrez.c
+++ xadrez.c
@@ -1,40 +1,64 @@
 #include <stdio.h>
 
 
+void recursivotorre(int numero){ //criação da função recursiva da torre (mestre)
+    if(numero > 0){
+        printf("Direita. \n");
+        recursivotorre(numero - 1);
+    }
+}
+
+
+void recursivobispo(int n){ //criação função recursiva do bispo (mestre)
+    if (n > 0) { //uso de loops aninhados
+        for (int i = 0; i < 1; i++) { //loop externo
+            printf("Cima. \n");
+
+
+            for (int j = 0; j < 1; j++) { //loop interno
+                printf("Direita. \n");
+            }
+        }
+        recursivobispo(n - 1);
+    }
+}
+
+
+void recursivorainha(int num){ //criação função recursiva da rainha (mestre)
+    if(num > 0){
+        printf("Esquerda. \n");
+        recursivorainha(num - 1);
+    }
+}
+
+
 int main() {
 
 
-    int torre, bispo = 0, rainha = 0; //variaveis com nomes das peças (novato)
-    int cavalo, movimentocavalo = 0; //variaveis (aventureiro)
-    
-
-    printf("Movimento da Torre: \n"); //movimentação da torre usando for (novato)
-    for(torre = 0; torre < 5; torre++) {
-        printf("Direita. \n");
-    }
+    int torre = 5, bispo = 5, rainha = 8; //variaveis com nomes das peças (novato)
+    int cavalo, movimentocavalo; //variaveis (aventureiro)
 
 
-    printf("\nMovimento do Bispo: \n"); //movimentação do bispo usando while
-    while(bispo < 5){
-        printf("Cima e direita. \n");
-        bispo++;
-    }
+    printf("Movimento da Torre: \n"); //movimentação da torre (mestre)
+    recursivotorre(torre);
 
 
-    printf("\nMovimento da Rainha: \n"); //movimentação da rainha usando do while
-    do{
-        printf("Esquerda. \n");
-        rainha++;
-    } while(rainha < 8);
+    printf("\nMovimento do Bispo: \n"); //movimentação do bispo (mestre)
+    recursivobispo(bispo);
+
+
+    printf("\nMovimento da Rainha: \n"); //movimentação da rainha (mestre)
+    recursivorainha(rainha);
 
     
-    printf("\nMovimento do Cavalo: \n"); //movimentação do cavalo usando loops aninhados (aventureiro)
-    for(cavalo = 0; cavalo < 1; cavalo++){
+    printf("\nMovimento do Cavalo: \n"); //movimentação do cavalo usando loops aninhados, múltiplas variáveis e comandos de controle de fluxo (mestre)
+    for(cavalo = 0, movimentocavalo = 0; cavalo < 1; cavalo++){
         while(movimentocavalo < 2){
-            printf("Baixo. \n");
+            printf("Cima. \n");
             movimentocavalo++;
         }
-        printf("Esquerda. \n");
+        printf("Direita. \n");
+        break;
     }
 
 
